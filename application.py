bfe8a40882dd485db3b4033db55a8f6c3a07b2d5
Ian van Dijk <idkvandijk@gmail.com>
2018-05-08 00:20:31 -0400

    first commit

---
+++ application.py
@@ -0,0 +1,34 @@
+import time
+import json
+from pprint import pprint
+
+from flask import Flask, jsonify, request
+
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+#example API function
+#curl -d @"data.json" -X POST http://localhost:5000/num/3 -H "Content-Type: application/json"
+
+@application.route('/num/<int>', methods=['GET', 'POST'])
+def thricer(int): 
+    if (request.method == 'POST'):
+		msg_json = request.get_json()
+		num1 = int(msg_json["num1"])
+		num2 = int(msg_json["num2"])
+		answer = 3*num1*num2
+		return jsonify({'answer': answer})
+
+
+    else:
+        return jsonify({'INSTRUCTIONS': 'include params num1 (int) and num2 (int)'})
+
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
